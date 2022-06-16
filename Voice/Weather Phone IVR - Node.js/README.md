ef3a0f5071e6becdc2a6d794298ca4ff39c2bdcb
niravcodes <mail@nirav.com.np>
2022-06-17 01:10:19 +0545

    Add Weather Phone IVR demo

---
+++ Voice/Weather Phone IVR - Node.js/README.md
@@ -0,0 +1,20 @@
+# Build your own Weather Phone IVR with Realtime API
+
+📖 [Read the full guide](https://developer.signalwire.com/apis/docs/weather-phone-in-nodejs-with-signalwire-realtime-api)
+
+This code sample is a simple weather phone IVR application that uses the [SignalWire Realtime API](https://developer.signalwire.com/client-sdk/reference/rt-exports) to provide current weather report to the caller in Washington DC either as a phone call or text.
+
+## To run this sample
+
+1. Fill in the Project ID and the API token from your SignalWire dashboard in the `.env.sample` file.
+2. Buy a number from the SignalWire Dashboard, configure it to use Relay with context `office`.
+3. Add that number to the `.env.sample` file under `PHONE_NUMBER`.
+4. Rename the `.env.sample` file to `.env`.
+5. Run this with `yarn install` then `yarn start` in the project directory.
+   Alternatively, you can use Docker instead:
+
+   a. `docker build . -t weather-ivr` to build the container using the dockerfile already in the repo.
+
+   b. `docker run -d weather-ivr` to run it.
+
+For more information on how to run this sample, or for a more detailed guide, please read the [guide](https://developer.signalwire.com/apis/docs/weather-phone-in-nodejs-with-signalwire-realtime-api).
---
+++ Voice/Weather Phone IVR - Node.js/index.js
@@ -0,0 +1,175 @@
+require("dotenv").config();
+const parsePhoneNumber = require("libphonenumber-js");
+const axios = require("axios");
+let sw = require("@signalwire/realtime-api");
+let { Voice, Messaging } = sw;
+
+const DC_WEATHER_PHONE = "+12025891212";
+const PHONE_NUMBER = process.env.PHONE_NUMBER;
+
+const realtimeConfig = {
+  project: process.env.PROJECT_ID,
+  token: process.env.API_TOKEN,
+  contexts: ["office"],
+};
+
+const messageClient = new Messaging.Client(realtimeConfig);
+const voiceClient = new Voice.Client(realtimeConfig);
+
+console.log("Waiting for calls...");
+voiceClient.on("call.received", async (call) => {
+  console.log("Got a call from", call.from, "to number", call.to);
+  await call.answer();
+  console.log("Inbound call answered");
+  try {
+    let welcome = await call.playTTS({
+      text: "Hello! Welcome to Knee Rub's Weather Helpline.",
+      gender: "male",
+    });
+    await welcome.waitForEnded();
+    console.log("Welcome text said");
+
+    let digits, terminator, type;
+    const cmdPrompt = await call.promptTTS({
+      text: "Please enter 1 for Washington weather, 2 for washington weather message, 3 to play rain dance, 4 to send rain dance",
+      digits: {
+        max: 1,
+        digitTimeout: 15,
+      },
+    });
+    let cmdResult = await cmdPrompt.waitForResult();
+    type = cmdResult.type;
+    digits = cmdResult.digits;
+    terminator = cmdResult.terminator;
+    console.log(
+      "Prompted for digits, received digits",
+      type,
+      digits,
+      terminator
+    );
+
+    if (digits === "1") {
+      await call.connectPhone({
+        from: call.from,
+        to: DC_WEATHER_PHONE,
+        timeout: 30,
+        ringback: new Voice.Playlist().add(
+          Voice.Playlist.TTS({
+            text: "ring. ring. ring. ring. ring. ring. ring. ring",
+          })
+        ),
+      });
+      console.log("Connecting to DC weather phone ...");
+      await call.waitUntilConnected();
+      console.log("Connected");
+    } else if (digits === "2") {
+      let place = "Washington DC";
+      console.log(`Sending message about weather of ${place}`);
+      let weather = await getWeatherFromOpenWeatherMap(place);
+      let message = `${place} weather: ${
+        weather.weather[0].description
+      }. Temperature: ${(weather.main.temp - 273).toFixed(2)}°C`;
+      console.log(message, "being sent to number", call.to);
+      try {
+        await messageClient.send({
+          from: PHONE_NUMBER,
+          to: call.to,
+          body: message,
+        });
+      } catch (e) {
+        let pb = await call.playTTS({
+          text:
+            "Sorry, I couldn't send the message." +
+            (e?.data?.from_number[0] ?? " ") +
+            " I will say the contents here. " +
+            message,
+        });
+        await pb.waitForEnded();
+      }
+    } else if (digits === "3") {
+      console.log("Sending rain dance song");
+      const rainDance = await call.playAudio({
+        url: "https://swrooms.com/rain.mp3",
+      });
+      await rainDance.waitForEnded();
+    } else if (digits === "4") {
+      console.log("Sending rain song to your friend");
+      const prompt = await call.promptTTS({
+        text: "Please enter your friend's number then dial #. Please use the international format, but skip the plus sign.",
+        digits: {
+          max: 15,
+          digitTimeout: 15,
+          terminators: "#",
+        },
+      });
+      const { type, digits, terminator } = await prompt.waitForResult();
+      console.log(
+        "Prompted for digits, received digits",
+        type,
+        digits,
+        terminator
+      );
+
+      let e164number;
+      number = parsePhoneNumber("+" + digits);
+      usnumber = parsePhoneNumber(digits, "US");
+      if (number && number.isValid()) {
+        e164number = number.number;
+      } else if (usnumber && usnumber.isValid()) {
+        e164number = usnumber.number;
+      }
+      console.log("Calling", e164number);
+      if (e164number) {
+        console.log("Calling with rain dance");
+        callWithRainDance(e164number);
+      } else {
+        console.log("Invalid number", digits);
+        let pb = await call.playTTS({
+          gender: "male",
+          text: "The number is invalid. Bye",
+        });
+        await pb.waitForEnded();
+      }
+    }
+    console.log("Hanging up");
+    await call.hangup();
+  } catch (error) {
+    console.error("Either call hung up by user, or some other error: ", error);
+  }
+});
+
+async function callWithRainDance(number) {
+  try {
+    console.log("Sending Call");
+    let call = await voiceClient.dialPhone({
+      from: PHONE_NUMBER,
+      to: number,
+      timeout: 30,
+    });
+    console.log("sending rain dance song");
+    const rainDance = await call.playAudio({
+      url: "https://swrooms.com/rain.mp3",
+    });
+    await rainDance.waitForEnded();
+    await call.hangup();
+  } catch (e) {
+    console.log("Call not answered.", e);
+  }
+}
+
+async function getWeatherFromOpenWeatherMap(location) {
+  let latlong;
+  if (location === "Washington DC") latlong = "lat=38.9072&lon=-77.0367";
+  else latlong = "lat=0&lon=0";
+  let weather;
+  try {
+    weather = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?${latlong}&appid=c0fedc735ee57d142c09bf7bf8ed8f04`
+    );
+  } catch (e) {
+    console.log(e);
+    return { weather: [{ description: "error" }], main: { temp: 0 } };
+  }
+  console.log(weather.data);
+  return weather.data;
+}

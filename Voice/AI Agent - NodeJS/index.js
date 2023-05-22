b070a5a443e50665a367d79c2fa35d0c0519eb7c
Renae Sowald <renaesowald@gmail.com>
2023-05-21 23:14:10 -0700

    update transfer function

--- Voice/AI Agent - NodeJS/index.js
+++ Voice/AI Agent - NodeJS/index.js
@@ -1,7 +1,9 @@
 import * as dotenv from "dotenv";
+import fetch from "node-fetch";
 dotenv.config();
 import express from "express";
 import { RestClient } from "@signalwire/compatibility-api";
+const host = "https://a64c6d6107f3.ngrok.app";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -13,51 +15,86 @@ app.post("/", (req, res) => {
   const agent = connect.ai({
     voice: "en-US-Neural2-D",
   });
-  agent.setPostPromptURL("https://b66f5d6f3366.ngrok.app/response");
+  agent.setPostPromptURL(host + "/response");
   agent.prompt(
     {
       confidence: 0.4,
       frequencyPenalty: 0.3,
     },
-    "You are Franklin's assistant, Sam. Your job is to answer phone calls and collect messages for Franklin. You may also answer questions about weather and time only by using the swaig functions provided. Start by introducing yourself, then let the caller know that Franklin is not available. You can reassure the caller that Franklin will get in touch as soon as possible. Ask for the user's name and silently wait 2 seconds for user response. Then ask for a phone number and wait 2 seconds for caller response. Then offer to collect a message. Silently wait 2 seconds for user response. If they do not want to leave a message, ask them to call at another time. After collecting the message, do not wait for the user to end the conversation: say goodbye and hang up the call. Be sure to hang up the call at the end of every conversation."
+    `You are the CEO's assistant.
+    Your job is to answer phone calls and collect messages or transfer the caller to a human agent. 
+    The CEO is not available now. Offer to take a message or transfer the caller to live agent.
+    You are able to transfer a call to support or sales. 
+    Here is a list of known numbers that you can transfer the caller to. Do not ask the caller for these numbers. Just transfer to the caller.
+    | Name    | Phone Number    |
+    | --------| --------------- |
+    | Sales   | 12057937849     |
+    | Support | 12057966865     |
+    If the caller would like to leave a message for the CEO, ask for the user's name, phone number, and their message. 
+    After collecting the message, do not wait for the user to end the conversation: say goodbye and hang up the call. 
+    Be sure to hang up the call at the end of every conversation.`
   );
-  agent.postPrompt(`Summarize the message as a valid anonymous json object by filling the upper case placeholders in this template:
-  {"contact_info": {"name": "CONTACT_NAME","number": "CONTACT_PHONE"}, "message": "MESSAGE"}`);
+  agent.postPrompt(`If the caller chooses to leave a message, summarize the message as a valid anonymous json object by filling the upper case placeholders in this template:
+  {"contact_info": {"name": "CONTACT_NAME","number": "CONTACT_PHONE"}, "message": "MESSAGE"}
+  If you use the transfer function, summarize your conversation including the phone number you transferred to.`);
 
   const swaig = agent.swaig();
 
   const transferFn = swaig.function({ name: "transfer" });
-  transferFn.setPurpose("TO DO");
-  transferFn.setArgument("TO DO: the number to call in std format");
+  transferFn.setPurpose("use this when a request for a transfer is made");
+  transferFn.setArgument(
+    "the 10-digit phone number that matches the name given in the transfer request"
+  );
   transferFn.setWebHookURL(
-    `https://b66f5d6f3366.ngrok.app/function?CallSid=${encodeURIComponent(
-      req.body.CallSid
-    )}`
+    `${host}/function?CallSid=${encodeURIComponent(req.body.CallSid)}`
   );
 
-  console.log(response.toString());
+  console.log("got the prompt");
 
   res.set("Content-Type", "text/xml");
   res.send(response.toString());
 });
 
 app.post("/function", (req, res) => {
-  console.log(req.body);
+  console.log("function endpoint hit: " + req.body);
   const callSid = req.query.CallSid;
 
   if (req.body.function === "transfer") {
-    // curl -L -X POST "https://undefined.signalwire.com/api/laml/2010-04-01/Accounts/PROJECT_ID/Calls/${encodeURIComponent(callSid)}" \
-    // -H 'Content-Type: application/x-www-form-urlencoded' \
-    // -H 'Accept: application/json' \
-    // -u 'projectid:token' \
-    // --data-urlencode "Url=https://b66f5d6f3366.ngrok.app/transfer?number=${encodeURIComponent(req.body.argument)}" \
-    // --data-urlencode 'Method=POST'
+    fetch(
+      `https://${
+        process.env.SIGNALWIRE_SPACE_URL
+      }/api/laml/2010-04-01/Accounts/${
+        process.env.PROJECT_ID
+      }/Calls/${encodeURIComponent(callSid)}`,
+      {
+        method: "POST",
+        headers: {
+          Authentication:
+            "Basic" +
+            Buffer.from(
+              process.env.PROJECT_ID + ":" + process.env.API_TOKEN,
+              "base64"
+            ),
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Url: `${host}/transfer?number=${encodeURIComponent(
+            req.body.argument
+          )}`,
+          Method: "POST",
+        }),
+      }
+    );
 
+    console.log(response.toString());
+    res.set("Content-Type", "text/xml");
+    res.send(response.toString());
     res.json({ response: "connecting" });
   }
 });
 
 app.post("/transfer", (req, res) => {
+  console.log("transfer endpoint hit");
   const number = req.query.number;
 
   const response = new RestClient.LaML.VoiceResponse();
@@ -68,6 +105,10 @@ app.post("/transfer", (req, res) => {
 
   res.set("Content-Type", "text/xml");
   res.send(response.toString());
+});
+
+app.post("/response", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(process.env.PORT || 3000, () => {

7aad02071a09bb5bffe0a22822b85e03af95e605
Daniele Di Sarli <danieleds0@gmail.com>
2023-06-08 15:36:23 +0200

    wrap comments

--- Voice/AI Agent with NodeJS/index.js
+++ Voice/AI Agent with NodeJS/index.js
@@ -14,7 +14,8 @@ const ADDRESS_BOOK = {
   support: process.env.SUPPORT_NUMBER,
 };
 
-// This route contains all of the information the AI agent will need for the conversation.
+// This route contains all of the information the AI agent will need for the
+// conversation.
 app.post("/", (req, res) => {
   const response = new RestClient.LaML.VoiceResponse();
   const connect = response.connect();
@@ -23,7 +24,8 @@ app.post("/", (req, res) => {
     voice: "en-US-Neural2-D",
   });
   agent.setPostPromptURL("/summary");
-  // Give the AI agent its role. Be as specific as possible. Do not include any sensitive information because the AI agent will share it.
+  // Give the AI agent its role. Be as specific as possible. Do not include any
+  // sensitive information because the AI agent will share it.
   agent.prompt(
     {
       confidence: 0.4,
@@ -39,11 +41,13 @@ app.post("/", (req, res) => {
     After collecting the message, do not wait for the user to end the conversation: say goodbye and hang up the call. 
     Be sure to hang up the call at the end of every conversation.`
   );
-  // Instruct the AI agent on what information to send about this call when it is complete.
+  // Instruct the AI agent on what information to send about this call when it
+  // is complete.
   agent.postPrompt(`Return a valid anonymous json object by replacing the uppercase placeholders in the following template with the caller's information.
   {"contact_name": "CONTACT_NAME", "message": "MESSAGE"}`);
 
-  // SWAIG allows us to define custom external functions for the AI agent to use. Name, purpose, argument, and webhook should be set as follows.
+  // SWAIG allows us to define custom external functions for the AI agent to
+  // use. Name, purpose, argument, and webhook should be set as follows.
   const swaig = agent.swaig();
 
   // Additional functions can be added using this same pattern.
@@ -62,13 +66,15 @@ app.post("/", (req, res) => {
   res.send(response.toString());
 });
 
-// The AI agent will call this route if the caller asks requests an external action that has been defined by a SWAIG function.
+// The AI agent will call this route if the caller asks requests an external
+// action that has been defined by a SWAIG function.
 app.post("/function", async (req, res) => {
   console.log(`AI agent has invoked the ${req.body.function} function`);
   const callSid = req.query.CallSid;
 
   if (req.body.function === "transfer") {
-    // Get the destination name from the parameters (either "sales" or "support")
+    // Get the destination name from the parameters (either "sales" or
+    // "support")
     const destination =
       req.body.argument.parsed.destination ??
       req.body.argument.parsed[0].destination;
@@ -114,7 +120,8 @@ app.post("/function", async (req, res) => {
   }
 });
 
-// This is a helper route that will build XML and send it to the API to connect the caller with a new number.
+// This is a helper route that will build XML and send it to the API to connect
+// the caller with a new number.
 app.post("/transfer", (req, res) => {
   const number = req.query.number;
 
@@ -130,7 +137,8 @@ app.post("/transfer", (req, res) => {
   res.send(response.toString());
 });
 
-// This route receives final information from the AI agent in response to the `postPrompt` that was set in the default route.
+// This route receives final information from the AI agent in response to the
+// `postPrompt` that was set in the default route.
 app.post("/summary", (req, res) => {
   console.log("Call from " + req.body.caller_id_number);
   if (req.body.post_prompt_data.parsed) {
